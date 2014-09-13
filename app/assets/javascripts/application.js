11adea616cdb84577b9151420d42a5ef25e31e2d
Marc Mentis <mmentis@optonline.net>
2014-09-13 17:35:05 -0400

    TURN OFF TURBOLINKS No JS YET

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,8 @@
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
+//       require turbolinks
+//
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
 //
@@ -15,7 +17,7 @@
 //= require foundation
 //= require dataTables/jquery.dataTables
 //= require dataTables/jquery.dataTables.foundation
-//= require turbolinks
+
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
